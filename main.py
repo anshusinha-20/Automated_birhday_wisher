d29a977169e804ee14b8b2e4c21df1bb9c7c7ed9
Anshu Sinha <anshujuly2@gmail.com>
2023-04-12 18:37:07 +0530

    Add a code to send automated birthday wish email

--- main.py
+++ main.py
@@ -93,3 +93,79 @@
 #             to_addrs=receiver,
 #             msg=f"Subject:Wednesday motivational quote!\n\n{random.choice(quotesList)}",
 #         )
+
+# ------------------------------------------------------------
+
+## day32: automated birthday wisher
+
+# 1. Update the birthdays.csv
+
+# 2. Check if today matches a birthday in the birthdays.csv
+
+# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
+
+# 4. Send the letter generated in step 3 to that person's email address.
+
+
+# -------------------- MODULES --------------------
+"""imported smtplib module"""
+import smtplib
+
+"""imported pandas module"""
+import pandas
+
+"""imported datetime module as dt"""
+import datetime as dt
+
+"""imported random module"""
+import random
+
+
+# -------------------- GETTING DATA --------------------
+"""variable to store the csv data"""
+data = pandas.read_csv("birthdays.csv")
+
+"""dictionary to store data"""
+birthdayDict = {(dataRow.month, dataRow.day): dataRow for (index, dataRow) in data.iterrows()}
+
+
+# -------------------- WORKING WITH DATES --------------------
+now = dt.datetime.today()
+
+"""variable to store month"""
+t_month = now.month
+
+"""variable to store year"""
+t_day = now.day
+
+"""tuple to store month and day"""
+t_tuple = (t_month, t_day)
+
+
+# -------------------- WORKING WITH EMAILS --------------------
+"""variable to store email address and password"""
+myEmail = "gpython72@gmail.com"
+password = "blxfzquksftguoxk"
+
+with smtplib.SMTP("smtp.gmail.com", port=587, timeout=25) as connection:
+    connection.starttls()
+    connection.login(
+        user=myEmail, password=password
+    )
+
+    for birthday in birthdayDict:
+        if birthday == t_tuple:
+            birthdayPerson = birthdayDict[birthday]["name"]
+            birthdayPersonEmail = birthdayDict[birthday]["email"]
+            randomNum = random.randint(1, 3)
+            with open(f"letter_templates/letter_{randomNum}.txt") as file:
+                letter = file.read()
+                birthdayLetter = letter.replace("[NAME]", birthdayPerson)
+
+    connection.sendmail(
+        from_addr=myEmail,
+        to_addrs=birthdayPersonEmail,
+        msg=f"Subject:Happy birthday {birthdayPerson}!\n\n{birthdayLetter}"
+    )
+
+
