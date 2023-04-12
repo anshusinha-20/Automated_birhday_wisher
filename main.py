74452ef2be5a0a630cc7e77ff2bf128a32202b6a
Anshu Sinha <anshujuly2@gmail.com>
2023-04-12 14:30:32 +0530

    Add a code to work with the datetime module

--- main.py
+++ main.py
@@ -1,31 +1,47 @@
-"""imported smtplib module"""
-import smtplib
+# """imported smtplib module"""
+# import smtplib
+#
+# """variable to store email address and password"""
+# myEmail = "gpython72@gmail.com"
+# password = "blxfzquksftguoxk"
+#
+# """variable to store receiver's address"""
+# receiver = "anshujuly26@yahoo.com"
+#
+# with smtplib.SMTP(
+#     "smtp.gmail.com",
+#     port=587,
+#     timeout=25
+# ) as connection:
+#
+#     """securing our connection"""
+#     connection.starttls()
+#
+#     """login"""
+#     connection.login(
+#         user=myEmail,
+#         password= password
+#     )
+#
+#     """send mail"""
+#     connection.sendmail(
+#         from_addr=myEmail,
+#         to_addrs=receiver,
+#         msg="Subject:Hi!\n\nThis is the new body of the message."
+#     )
 
-"""variable to store email address and password"""
-myEmail = "gpython72@gmail.com"
-password = "blxfzquksftguoxk"
+## working with the datetime module
 
-"""variable to store receiver's address"""
-receiver = "anshujuly26@yahoo.com"
+"""imported datetime module as dt"""
+import datetime as dt
 
-with smtplib.SMTP(
-    "smtp.gmail.com",
-    port=587,
-    timeout=25
-) as connection:
+"""created now object"""
+now = dt.datetime.now()
+year = now.year
+month = now.month
+date = now.date()
+dayOfTheWeek = now.weekday()
+print(date)
 
-    """securing our connection"""
-    connection.starttls()
-
-    """login"""
-    connection.login(
-        user=myEmail,
-        password= password
-    )
-
-    """send mail"""
-    connection.sendmail(
-        from_addr=myEmail,
-        to_addrs=receiver,
-        msg="Subject:Hi!\n\nThis is the new body of the message."
-    )
+dateOfBirth = dt.datetime(year=2003, month=7, day=26)
+print(dateOfBirth.date())
